940816fc26c7b044a23ded4db261f96fd04c3d80
Waqas Ali <waqas.abbasi@outlook.com>
2021-07-15 17:47:55 +0800

    upgrade gatsby to v3

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,11 @@ module.exports = {
   siteMetadata: {
     title: `Waqas Ali`,
     description: `Hi, I am Waqas and this is my personal website describing my projects, experience and achievements so far.`,
-    lastUpdated: `13 March 2020`,
+    lastUpdated: `15 July 2021`,
     twitter: "@totally_waqas",
     github: "WaqasAliAbbasi",
     linkedin: "waqasaliabbasi",
-    email: "waqas.abbasi@outlook.com"
+    email: "waqas.abbasi@outlook.com",
   },
   plugins: [
     `gatsby-plugin-typescript`,
@@ -15,16 +15,16 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-46712607-5",
-        head: true
-      }
+        head: true,
+      },
     },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`
-      }
+        path: `${__dirname}/src/images`,
+      },
     },
     `gatsby-plugin-chakra-ui`,
     `gatsby-transformer-sharp`,
@@ -39,22 +39,22 @@ module.exports = {
         theme_color: `#663399`,
         theme_color_in_head: false,
         display: `minimal-ui`,
-        icon: `src/images/waqas-ali.jpg` // This path is relative to the root of the site.
-      }
+        icon: `src/images/waqas-ali.jpg`, // This path is relative to the root of the site.
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `projects`,
-        path: `${__dirname}/src/projects`
-      }
+        path: `${__dirname}/src/projects`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/src/posts`
-      }
+        path: `${__dirname}/src/posts`,
+      },
     },
     {
       resolve: `gatsby-plugin-mdx`,
@@ -64,15 +64,15 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590
-            }
-          }
-        ]
-      }
+              maxWidth: 590,
+            },
+          },
+        ],
+      },
     },
-    `gatsby-source-local-git`
+    `gatsby-source-local-git`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ]
+  ],
 };
