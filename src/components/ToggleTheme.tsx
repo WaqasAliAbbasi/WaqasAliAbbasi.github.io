18cf7cc47804beda06c92cf8cadf91677756b6e6
Waqas Ali <waqas.abbasi@outlook.com>
2023-08-01 15:00:39 +0800

    Check whether toggle theme is mounted client side

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -2,22 +2,33 @@
 
 import { useTheme } from "next-themes";
 import { SunIcon, MoonIcon } from "@heroicons/react/24/outline";
+import { useEffect, useState } from "react";
 
 export const ToggleTheme = () => {
-  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const { systemTheme, theme, setTheme } = useTheme();
 
-  const Icon = theme === "light" ? MoonIcon : SunIcon;
-  const toggleTheme = () => setTheme(theme === "light" ? "dark" : "light");
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  const currentTheme = theme === "system" ? systemTheme : theme;
+  const toggleTheme = () =>
+    setTheme(currentTheme === "light" ? "dark" : "light");
+  const Icon = currentTheme === "light" ? MoonIcon : SunIcon;
 
   return (
     <button
       title="Toggle Theme"
       onClick={toggleTheme}
       type="button"
-      className="rounded outline outline-1 p-1 dark:hover:bg-gray-700 hover:bg-gray-100 "
-      suppressHydrationWarning
+      className="rounded outline outline-1 p-1 dark:hover:bg-gray-700 hover:bg-gray-100"
     >
-      <Icon className="h-4 w-4" aria-hidden="true" />
+      <Icon className="h-4 w-4" />
     </button>
   );
 };
